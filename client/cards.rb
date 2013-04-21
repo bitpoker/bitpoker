1af3e0ca1bffa747036413f032322d73d73042da
al4nw31 <xfx888@hotmail.com>
2013-04-21 12:15:15 -0700

    add Card and Suit class

--- client/cards.rb
+++ client/cards.rb
@@ -1,2 +1,15 @@
-CARD_NUMS = [2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K, A]
-CARD_SUITS = [D, C, H, S]
+CARD_NUMS = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
+CARD_SUITS = [:D, :C, :H, :S]
+
+class Suit
+	
+end
+
+class Card
+	attr_accessor :num, :suit
+
+	def initialize(num, suit)
+		self.num = num
+		self.suit = suit
+	end
+end
