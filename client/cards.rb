f4f1f746c05756442830173e7c77ad99836b7723
al4nw31 <xfx888@hotmail.com>
2013-04-21 12:54:38 -0700

    cardStrength for quick comparison of cards

--- client/cards.rb
+++ client/cards.rb
@@ -1,15 +1,19 @@
 CARD_NUMS = [2, 3, 4, 5, 6, 7, 8, 9, 10, :J, :Q, :K, :A]
 CARD_SUITS = [:D, :C, :H, :S]
 
-class Suit
-	
-end
-
 class Card
 	attr_accessor :num, :suit
 
 	def initialize(num, suit)
-		self.num = num
-		self.suit = suit
+		@num = num
+		@suit = suit
+	end
+
+	def cardStrength()
+		return (CARD_NUMS.index(num) * 10) + (CARD_SUITS.index(suit))
 	end
 end
+
+card = Card.new :K, :S
+
+puts card.cardStrength()
