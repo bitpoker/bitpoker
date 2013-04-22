be33b3f1631d00367a44ea358d4f2f7f98a78b8e
William Meng <wlmeng11@gmail.com>
2013-04-21 18:17:13 -0700

    Using postgre

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Bitpoker::Application.config.secret_token = '98e3837401478cb83c0f5116073aefff76605b9e082587fc62003fa684a39362d08b890374f931e31f698ee4dabf565a13dcf8474c65ffb05f1c3f91e7c8845a'
+Bitpoker::Application.config.secret_token = '6cf136e970e7f554e505707fb59835d1929c3ecb75031919d87205349273b2aa87f27d23676daec906aa85c3a71b8b9918c762f821d3f590eaec9378bc0bb2d0'
