873740ce4a3c6977d18cb1da7fa5cec8df796ae1
William Meng <wlmeng11@gmail.com>
2013-04-21 18:03:43 -0700

    Rails init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitpoker::Application.config.secret_token = '98e3837401478cb83c0f5116073aefff76605b9e082587fc62003fa684a39362d08b890374f931e31f698ee4dabf565a13dcf8474c65ffb05f1c3f91e7c8845a'
