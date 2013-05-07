7a94ac30ebe7aa51dd64b7d2cc8e0b15f84386f6
William Meng <wlmeng11@gmail.com>
2013-05-07 16:56:38 -0700

    Refining of registration checks + pretty buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ActiveRecord::Base
-  acts_as_authentic
   attr_accessible :username, :email, :password, :password_confirmation
 
-  validates :email, :presence=> true
-  validates :username, :presence => true
-  #validates :crypted_password, :presence => true
-  
+  acts_as_authentic do |c|
+    # set minimum password value to 8
+    c.merge_validates_length_of_password_field_options({:minimum => 8})
+  end
+
+  # regex to require good password
+  validates_format_of :password, :with => /^(?=.\d)(?=.([a-z]|[A-Z]))([\x20-\x7E]){8,40}$/, :if => :require_password?, :message => "must include one number, one letter, and be between 6 and 40 characters"
+
 end
