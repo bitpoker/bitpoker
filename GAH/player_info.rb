4a7fe98ca2a51143a24e6e0036f5c517da6c0a16
liliatangxy <liliatangxy@gmail.com>
2013-05-08 12:32:02 -0600

    wrote extension of user_info for use in playing

--- GAH/player_info.rb
+++ GAH/player_info.rb
@@ -1,18 +1,25 @@
-class Player
+require 'cards.rb'
 
-	@@Characters username
-	@@Number money
+class User_Info
+	attr_accessor :username
+	attr_accessor :money
 
 	def initialize(username, money)
 		@username = username
 		@money = money	
 	end
 
-	def getName()
+	def getName
 		return @username
 	end
 	
-	def getMoney()
+	def getMoney
 		return @money
 	end
 end	
+
+class Player < User_Info
+	attr_accessor :hand
+	
+	@hand = Hand.new
+end
