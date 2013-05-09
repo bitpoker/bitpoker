1fd8b34cf1e10b5af389729426acca39575feacc
William Meng <wlmeng11@gmail.com>
2013-05-08 21:24:19 -0700

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130507212155) do
+ActiveRecord::Schema.define(:version => 20130508001036) do
+
+  create_table "user_sessions", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "username"
